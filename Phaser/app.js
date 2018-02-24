e80b8d717bcb0b20beb5610d1755bc38b874e1f7
Jamesrsum <jamesrsum@gmail.com>
2018-02-24 14:34:24 +0800

    project 6

---
+++ Phaser/app.js
@@ -0,0 +1,21 @@
+var game = new Phaser.Game(800,600,Phaser.AUTO,'',
+	{preload:preload,create: create, update:update});
+
+
+var score = 0;
+var life = 3;
+
+function preload(){
+game.load.image('sky','assets/sky.png');
+game.load.image('ground','assets/platform.png')
+game.load.spritesheet('dude','assets/dude.png',32,48)
+game.load.spritesheet('baddie','assets/baddie.png',32,48)
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
